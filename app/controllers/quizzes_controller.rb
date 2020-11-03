9a44d13ab4fd8fef79d58f8ce47927185e22df8d
shofny <sho.fuyuyado0705@gmail.com>
2020-11-03 14:50:44 +0900

    問題投稿機能の実装完了

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,6 +1,27 @@
 class QuizzesController < ApplicationController
 
-def index
-end
+  def index
+
+  end
+  
+  def new
+    @quiz = Quiz.new
+    @quiz.choices.build
+    # 4.times{@quiz.choices.build}
+  end
+
+  def create
+  @quiz = Quiz.new(quiz_params)
+  @quiz.save
+  redirect_to root_path
+
+    
+  end
+
+  private
+
+  def quiz_params
+    params.require(:quiz).permit(:quiz,choices_attributes:[:choice, :answer_is]).merge(user_id: current_user.id)
+  end
 
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,2 +1,7 @@
 class Quiz < ApplicationRecord
+
+  belongs_to :user
+  has_many :choices
+  accepts_nested_attributes_for :choices
+
 end
