162ca2bf50e1d5bc9abf02e1070a49c0f3d43715
shofny <sho.fuyuyado0705@gmail.com>
2020-11-04 22:36:54 +0900

    クイズの正誤判定・次の問題への移行の記述

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,8 +1,16 @@
 class PlaysController < ApplicationController
 
   def index
-    @quizzes = Quiz.all
-    # @choices = Choice.all
+  end
+
+  def anwser
+    @anwser = params[:anwser].to_i
+    quiz_id = params[:quiz_id].to_i
+    q_no = quiz_id + 1
+    # if q_no == 6
+    #   最終結果のページに飛ぶ
+    # end
+    @quizzes = Quiz.find(q_no)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
-root to: "quizzes#index"
-resources :quizzes, except:[:index]
-resources :plays, only:[:index]
+  root to: "quizzes#index"
+  resources :quizzes, except:[:index]
+  resources :plays, only:[:index, :create] do
+    collection do
+      post 'anwser'
+    end
+  end
 
 end
