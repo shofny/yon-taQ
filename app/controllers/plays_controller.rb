9bbd61b2a6956942609e7a06dc44ea0f7e86f3fe
shofny <sho.fuyuyado0705@gmail.com>
2020-11-05 21:47:52 +0900

    クイズのランダムセレクト・1問目-2問目への遷移まで

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,16 +1,20 @@
 class PlaysController < ApplicationController
 
   def index
+    @rand_quiz = Quiz.order("RAND()").limit(3)
+    # binding.pry
   end
 
   def anwser
     @anwser = params[:anwser].to_i
-    quiz_id = params[:quiz_id].to_i
-    q_no = quiz_id + 1
-    # if q_no == 6
-    #   最終結果のページに飛ぶ
-    # end
-    @quizzes = Quiz.find(q_no)
+    quiz_no = params[:quiz_no].to_i
+    rand_quiz = params[:rand_quiz].split(" ").map(&:to_i)
+    q_no = quiz_no + 1
+    if q_no == 4
+      redirect_to root_path
+    end
+    binding.pry
+    @quizzes = Quiz.find(rand_quiz[quiz_no + 1])
   end
 
 end
