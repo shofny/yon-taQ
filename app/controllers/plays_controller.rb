60a4019c94833d9f7f8bbb59075141f897ff0495
shofny <sho.fuyuyado0705@gmail.com>
2020-11-06 23:30:43 +0900

    sessionを使って記述をし直し・ラジオボタンが1番目の選択肢に自動的に合うようにした

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -2,20 +2,27 @@ class PlaysController < ApplicationController
 
   def index
     @rand_quiz = Quiz.order("RAND()").limit(3)
-    # binding.pry
+    session[:rand_quiz] = @rand_quiz
+    session[:quiz_no] = 0
+    session[:correct_num] = 0
   end
 
   def anwser
-    @anwser = params[:anwser].to_i
-    quiz_no = params[:quiz_no].to_i
-    @rand_quiz = params[:rand_quiz].split(" ").map(&:to_i)
-    @q_no = quiz_no + 1
-    if (@q_no) >= 3
+  
+    session[:anwser] = params[:anwser].to_i
+    # quiz_no = params[:quiz_no].to_i
+    # # @correct_num = params[:corrct_num].to_i
+    # @rand_quiz = params[:rand_quiz].split(" ").map(&:to_i)
+    session[:quiz_no] += 1
+    if session[:quiz_no] >= 3
       redirect_to result_plays_path
     else
-    @quizzes = Quiz.find(@rand_quiz[@q_no])
+      @quizzes = session[:rand_quiz][session[:quiz_no]]
     end
 
+  end
+
+  def result
   end
 
 end
