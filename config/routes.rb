5ba95a11efcb117e3a3adc09df5206097e48ae68
shofny <sho.fuyuyado0705@gmail.com>
2020-11-13 15:32:37 +0900

    マイページ遷移

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "quizzes#index"
   resources :quizzes, except:[:index]
+  resources :users, only:[:show]
   resources :plays, only:[:index, :create] do
     collection do
       post 'anwser'
